6191a1a87d833c10312834216689c6179b70528b
dev-jens <baetenjens07@gmail.com>
2024-12-10 22:42:49 +0100

    day4

--- day4/day4.py
+++ day4/day4.py
@@ -1,16 +1,60 @@
-import re
+import os.path
+
+INPUT_PATH = os.path.join(os.path.dirname(__file__), "input.txt")
+
+directions = [
+    (-1, -1),  # diagonal left above
+    (-1, 0),  # above current letter
+    (-1, 1),  # diagonal right above
+    (0, -1),  # go left
+    (0, 1),  # go right
+    (1, -1),  # left diagonal under
+    (1, 0),  # under
+    (1, 1),  # right diagonal under
+]
+
 
 def main():
-    file_path = './day4/input.txt'
-    
-    with open(file_path, 'r') as file:
-        lines = file.read().strip().split("\n")
-   
-    for line in lines:
-        print(line)
-                    
-             
-                
 
-if __name__ == '__main__':
+    with open(INPUT_PATH, "r") as file:
+        lines = file.read().strip().split("\n")
+
+    ans = 0
+    col_len = len(lines)
+    row_len = len(lines[0])  # every line is same lenght
+    
+    for col in range(col_len):
+        for row in range(row_len):
+            for dir in directions:
+                if check_xmas(col,row,dir,lines):
+                    ans += 1
+    print(ans)
+
+def check_xmas(col, row, direction, lines):
+    
+    word_to_check = "XMAS"
+    dir_col = direction[0]
+    dir_row = direction[1]
+    col_len = len(lines)
+    row_len = len(lines[0])
+    
+    for index, letter in enumerate(word_to_check):
+        curr_col = col + index * dir_col
+        curr_row = row + index * dir_row
+        
+        if  not ( 0 <= curr_col < col_len  and 0 <= curr_row <  row_len):    
+            return False
+        
+        if lines[curr_col][curr_row] is not letter:
+           return False
+             
+    return True
+    
+        
+    
+    
+    
+    
+
+if __name__ == "__main__":
     main()
--- day4/day4_part2.py
+++ day4/day4_part2.py
@@ -1,31 +1,53 @@
-import re
+import os.path
+
+INPUT_PATH = os.path.join(os.path.dirname(__file__), "input.txt")
+
+directions = [
+    (-1, -1),  # diagonal left above
+    (-1, 0),  # above current letter
+    (-1, 1),  # diagonal right above
+    (0, -1),  # go left
+    (0, 1),  # go right
+    (1, -1),  # left diagonal under
+    (1, 0),  # under
+    (1, 1),  # right diagonal under
+]
+
 
 def main():
-    file_path = './day3/input.txt'
-    
-    with open(file_path, 'r') as file:
-        memory = file.read()
-        
-    matches = re.findall(r"(?:mul\((\d+),(\d+)\))|(do\(\)|don't\(\))", memory)
-    print(matches)
-    
-    sum_of_matches = 0
-    do_is_active = True
-    
-    for match in matches:
-        if match[2] == "" and do_is_active:
-            sum_of_matches += int(match[0]) * int(match[1])
-        else:
-            if match[2] == "do()":
-                do_is_active = True
-            else:
-                do_is_active = False
-        
-        
-    print(sum_of_matches)
-                    
-             
-                
 
-if __name__ == '__main__':
+    with open(INPUT_PATH, "r") as file:
+        lines = file.read().strip().split("\n")
+
+    ans = 0
+    col_len = len(lines)
+    row_len = len(lines[0])  # every line is same lenght
+    
+    for col in range(col_len):
+        for row in range(row_len):
+            for dir in directions:
+                if has_xmas(col,row,dir,lines):
+                    ans += 1
+    print(ans)
+
+def has_xmas(col, row, direction, lines):
+    
+    if not (1 <= col < col - 1 and 1 <= row< row - 1):
+        return False
+    if lines[col][row] != "A":
+        return False
+
+    # Check both diagonals
+    diag_1 = f"{lines[col-1][row-1]}{lines[col+1][row+1]}"
+    diag_2 = f"{lines[col-1][row+1]}{lines[col+1][row-1]}"
+
+    return diag_1 in ["MS", "SM"] and diag_2 in ["MS", "SM"]
+    
+        
+    
+    
+    
+    
+
+if __name__ == "__main__":
     main()
